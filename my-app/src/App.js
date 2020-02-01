3acab51ef820635f52870b859e1a3463f7065994
sdeewwf <sdeewcsgo@gmail.com>
2020-02-01 19:05:19 +0700

    LAB 3.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Monent from 'react-moment';
+import Moment from 'react-moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+  
+  componentDidMount() {
+    fetch('/api/todo')
+    .then(res=> res.json()).then(json => {
+      this.setState({
+        items: json,
+        isLoaded: true
+      });
+    })
+  }
+
+  render() {
+    const { isLoaded, items } = this.state;
+    console.log(items);
+
+    if(!isLoaded)
+      return <div>Loading...</div>;
+
+    return (
+      <div className= "App">
+        <h1>Задачи:</h1>
+        <div className="todos">
+            <ul>
+              {items.map(item=> (
+                <li key={item.id}>
+                  {item.text} | <Moment format="YYYY/MM/DD">{item.date}</Moment>
+                </li>
+              ))}
+            </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
